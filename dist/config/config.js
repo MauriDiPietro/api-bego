49c52b115b6ad207068b97c01725aa07564deb52
MauriDiPietro <dipietro.jm@gmail.com>
2023-08-01 13:36:06 -0300

    delete dist

--- dist/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-exports.default = {
-    PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI,
-    JWT_KEY: process.env.JWT_KEY || 'token',
-    API_KEY_GOOGLE: process.env.API_KEY_GOOGLE
-};
